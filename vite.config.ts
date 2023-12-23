e84f20ace6080e1e09e7028ae6908b34598fbe74
Ataru Ohto <ataruohto@gmail.com>
2023-12-23 22:18:00 +0900

    enabled msw browser

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  alias: {
-    src: "/src",
-  },
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  }
 })
