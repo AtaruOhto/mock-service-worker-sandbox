7344c5eba5eed5f6b0770a41c0d71fd84ee1b1cc
Ataru Ohto <ataruohto@gmail.com>
2023-12-25 14:26:32 +0900

    vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  test: {
+    environment: "happy-dom",
+  },
+});
+
